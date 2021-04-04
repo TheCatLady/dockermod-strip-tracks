d650e17badcfcc501af3086df5066586ab7b2a81
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2021-04-04 17:29:01 -0400

    Check for TMDb API key and make CONFIG_DIR configurable via env var

--- root/usr/local/bin/strip-tracks.py
+++ root/usr/local/bin/strip-tracks.py
@@ -12,12 +12,13 @@ import untangle
 
 def refresh_arr():
     if SERVER_TYPE == "radarr":
-        payload = {"name": "RefreshMovie", "movieIds": [ID]}
+        payload = {"name": "RefreshMovie", "movieIds": [MOVIE_ID]}
     elif SERVER_TYPE == "sonarr":
-        payload = {"name": "RefreshSeries", "seriesId": ID}
+        payload = {"name": "RefreshSeries", "seriesId": SERIES_ID}
 
     params = {"apikey": API_KEY}
     req = requests.post(f"http://localhost:{'7878' if SERVER_TYPE == 'radarr' else '8989'}/api/v3/command", params=params, json=payload)
+
     try:
         req.raise_for_status()
         return req.json()
@@ -34,15 +35,23 @@ log = logging.getLogger(f"{SERVER_TYPE}PostProcessing")
 
 EVENT_TYPE = os.environ.get(f"{SERVER_TYPE}_eventtype")
 if EVENT_TYPE != "Download":
-    log.info(f"Event type is not Download, but {EVENT_TYPE}. Nothing to do here. Exiting script.")
+    log.info(f"Event type is not Download, but {EVENT_TYPE}.")
     sys.exit(0)
+
+CONFIG_DIR = os.environ.get("CONFIG_DIR")
+if not CONFIG_DIR:
+    CONFIG_DIR = "/config"
+    os.environ["CONFIG_DIR"] = CONFIG_DIR
 
 API_KEY = os.environ.get("API_KEY")
 if not API_KEY:
-    API_KEY = untangle.parse('/config/config.xml').Config.ApiKey.cdata
+    API_KEY = untangle.parse(f"{CONFIG_DIR}/config.xml").Config.ApiKey.cdata
     os.environ["API_KEY"] = API_KEY
 
 TMDB_API_KEY = os.environ.get("TMDB_API_KEY")
+if not TMDB_API_KEY:
+    log.error(f"TMDB_API_KEY environment variable must be set.")
+    sys.exit(1)
 
 try:
     SUBTITLE_LANGUAGES = list(filter(str.strip, os.environ.get("SUBTITLE_LANGUAGES").lower().split(',')))
@@ -55,36 +64,45 @@ except Exception:
     EXCLUDED_KEYWORDS = []
 
 if SERVER_TYPE == "radarr":
-    ID = int(os.environ.get("radarr_movie_id"))
+    MOVIE_ID = int(os.environ.get("radarr_movie_id"))
     FILE_PATH = os.environ.get("radarr_moviefile_path")
     TMDB_ID = os.environ.get("radarr_movie_tmdbid")
+    SCENE_NAME = os.environ.get("radarr_moviefile_scenename")
+
 elif SERVER_TYPE == "sonarr":
-    ID = int(os.environ.get("sonarr_series_id"))
+    SERIES_ID = int(os.environ.get("sonarr_series_id"))
+    EPISODE_ID = int(os.environ.get("sonarr_episodefile_id"))
     FILE_PATH = os.environ.get("sonarr_episodefile_path")
     TVDB_ID = os.environ.get("sonarr_series_tvdbid")
+    SCENE_NAME = os.environ.get("sonarr_episodefile_scenename")
 
     params = {"api_key": TMDB_API_KEY, "external_source": "tvdb_id"}
     req = requests.get(f"https://api.themoviedb.org/3/find/{TVDB_ID}", params=params)
+
     try:
         req.raise_for_status()
+        tmdbData = req.json()
+        TMDB_ID = tmdbData["tv_results"][0]["id"]
     except HTTPError as e:
         log.error(e)
 
-    tmdbData = req.json()
-    TMDB_ID = tmdbData["tv_results"][0]["id"]
+    if not TMDB_ID:
+        log.error(f"Could not find TMDb ID for series with TVDB ID {TVDB_ID}.")
+        sys.exit(1)
 
 params = {"api_key": TMDB_API_KEY}
 req = requests.get(f"https://api.themoviedb.org/3/{'movie' if SERVER_TYPE == 'radarr' else 'tv'}/{TMDB_ID}", params=params)
+
 try:
     req.raise_for_status()
     tmdbData = req.json()
+
+    if "original_language" in tmdbData:
+        ORIGINAL_LANGUAGE = translate(tmdbData["original_language"])
+    else:
+        ORIGINAL_LANGUAGE = ""
 except Exception as e:
     log.error(e)
-
-if "original_language" in tmdbData:
-    ORIGINAL_LANGUAGE = translate(tmdbData["original_language"])
-else:
-    ORIGINAL_LANGUAGE = ""
 
 result = subprocess.run(["mkvmerge", "-J", FILE_PATH], check=True, capture_output=True, text=True)
 
@@ -122,18 +140,16 @@ for track in tracks:
                 if ("forced_track" in track["properties"] and track["properties"]["forced_track"]) or "track_name" not in track["properties"] or "sdh" not in track["properties"]["track_name"].lower() or ("language" in track["properties"] and track["properties"]["language"] not in nonForcedSubtitleLanguages):
                     subtitleTracks.append(str(track["id"]))
 
+                    # Keep track (pun not intended) of languages for which we already have non-forced track
                     if "language" in track["properties"] and "forced_track" not in track["properties"] or ("forced_track" in track["properties"] and not track["properties"]["forced_track"]):
                         nonForcedSubtitleLanguages.append(track["properties"]["language"])
 
 if videoTrack and audioTrack:
     log.info(f"Keeping video track {videoTrack}, audio track {audioTrack}, and subtitle track(s) {','.join(subtitleTracks)} for {FILE_PATH}")
 
-    orderedTracks = [videoTrack, audioTrack]
-    orderedTracks.extend(subtitleTracks)
-
     os.rename(FILE_PATH, f"{FILE_PATH}.old")
 
-    # Refresh here as a workaround for https://github.com/Sonarr/Sonarr/issues/3366
+    # Refresh as a workaround for https://github.com/Sonarr/Sonarr/issues/3366
     refresh_arr()
 
     mkvmerge = [
@@ -152,6 +168,10 @@ if videoTrack and audioTrack:
         mkvmerge.append("-S")
 
     mkvmerge.append(f"{FILE_PATH}.old")
+
+    # Re-order tracks so that video track is always ID 0, and audio track is always ID 1
+    orderedTracks = [videoTrack, audioTrack]
+    orderedTracks.extend(subtitleTracks)
     mkvmerge.extend(["--track-order", f"0:{',0:'.join(orderedTracks)}"])
 
     subprocess.run(mkvmerge, check=True)
@@ -162,30 +182,20 @@ refreshData = refresh_arr()
 
 if "id" in refreshData:
     COMMAND_ID = refreshData["id"]
-    refreshComplete = False
 
-    while not refreshComplete:
+    while True:
         time.sleep(5)
         params = {"apikey": API_KEY}
-        req = requests.get(f"http://localhost:{'7878' if SERVER_TYPE == 'radarr' else '8989'}/api/v3/command", params=params)
+        req = requests.get(f"http://localhost:{'7878' if SERVER_TYPE == 'radarr' else '8989'}/api/v3/command/{COMMAND_ID}", params=params)
 
         try:
             req.raise_for_status()
             response = req.json()
+
+            if response["status"].lower() == "completed":
+                break
         except Exception as e:
             log.error(e)
-            break
-
-        hasCommandId = False
-        for command in response:
-            if command["id"] == COMMAND_ID:
-                if command["status"] == "completed":
-                    refreshComplete = True
-
-                hasCommandId = True
-                break
-        
-        if not hasCommandId:
             break
 
 if os.environ.get("DISCORD_WEBHOOK"):
